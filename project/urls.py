2080a989d233c1d5b4ebf412a23a82c8cbc9f497
gabrieltrouve <gabrieltrouve5@gmail.com>
2025-11-18 07:27:06 +0100

    Update URL patterns and base template for improved navigation and routing

--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop/', include('shop.urls')),
+    path('', include('shop.urls')),
     path('sav/', include('sav.urls')),
 ]
